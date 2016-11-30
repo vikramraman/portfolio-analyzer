ce2a3a3bde245958df9ba5ae80c895e3debfcd21
Vikram Raman <vikram.raman@gmail.com>
2016-11-29 23:20:57 -0800

    Added expanded analysis

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,5 +1,8 @@
 import csvreader
 import helper
+import argparse
+import os
+import sys
 
 SYMBOL_IDX=0
 QTY_IDX=1
@@ -13,13 +16,23 @@ SHORT_TAX_RATE=.25
 CURRENT_SHORT_TAX_RATE=.28
 LONG_DAYS=365
 
-def analyze(rows):
+yearlyProfits = {}
+yearlyTaxedProfits = {}
+yearlyTransactions = {}
+totalBuy = 0
+totalSale = 0
+totalNetProfit = 0
+currentInvested = 0
+
+def _analyze(rows):
+    global totalBuy
+    global totalSale
+    global totalNetProfit
+    global currentInvested
+
     if not rows:
         raise "Invalid data input"
 
-    totalBuy = 0
-    totalSale = 0
-    totalTaxedProfit = 0
     for row in rows:
         symbol = row[SYMBOL_IDX]
         qty = float(row[QTY_IDX])
@@ -30,13 +43,37 @@ def analyze(rows):
 
         buyTotal = buyRate * qty
         saleTotal = saleRate * qty
+        year = helper.getYear(saleDate)
+
         profit = saleTotal - buyTotal
-        taxedProfit = _getTaxedProfit(profit, buyDate, saleDate)
+        netProfit = _getNetProfit(profit, buyDate, saleDate)
         totalBuy += buyTotal
         totalSale += saleTotal
-        totalTaxedProfit += taxedProfit
-        print symbol, qty, buyRate, saleRate, profit, taxedProfit
-    print totalBuy, totalSale, (totalSale-totalBuy), totalTaxedProfit
+        totalNetProfit += netProfit
+
+        if not saleDate:
+            currentInvested += buyTotal
+
+        _updateProfits(year, yearlyProfits, profit)
+        _updateProfits(year, yearlyTaxedProfits, netProfit)
+        _updateTransactions(buyDate, saleDate, yearlyTransactions)
+        print year, symbol, qty, buyRate, saleRate, profit, netProfit
+
+def _updateProfits(year, yearlyProfits, profit):
+    sum = yearlyProfits.get(year, 0)
+    yearlyProfits[year] = sum + round(profit, 2)
+
+def _updateTransactions(buyDate, saleDate, transDict):
+    buyYear = helper.getYear(buyDate)
+    saleYear = helper.getYear(saleDate)
+    _incrementTransaction(buyYear, 'bought', transDict)
+    _incrementTransaction(saleYear, 'sold', transDict)
+
+def _incrementTransaction(year, key, transDict):
+    dic = transDict.get(year, {})
+    count = dic.get(key, 0)
+    dic[key] = count + 1
+    transDict[year] = dic
 
 def _getTaxRate(buyDate, saleDate):
     daysHeld = helper.getDays(buyDate, saleDate)
@@ -45,13 +82,40 @@ def _getTaxRate(buyDate, saleDate):
     #TODO: compute correctly based on income bracket
     return SHORT_TAX_RATE
 
-def _getTaxedProfit(profit, buyDate, saleDate):
+def _getNetProfit(profit, buyDate, saleDate):
     taxRate = _getTaxRate(buyDate, saleDate)
     if profit <= 0:
         return profit
     return profit - (profit * taxRate)
 
+def doAnalyze(args):
+    csvDir = args.csvDir
+    if not os.path.isdir(csvDir):
+        print "Invalid input: Please enter a valid directory"
+        sys.exit(1)
+
+    for file in os.listdir(csvDir):
+        if file.endswith(".csv"):
+            data = csvreader.read(csvDir + "/" + file)
+            _analyze(data)
+    _pprintData()
+
+def _pprintData():
+    print "\nResults:\n"
+    totalProfit = totalSale - totalBuy
+    helper.printFloat("Total Cost", totalBuy)
+    helper.printFloat("Total Sale", totalSale)
+    helper.printFloat("Gross Profit", totalProfit)
+    helper.printFloat("Net Profit", totalNetProfit)
+    helper.printPercent("Gross profit %:", totalProfit, totalBuy)
+    helper.printPercent("Net profit %:", totalNetProfit, totalBuy, True)
+    helper.printFloat("Outstanding investments", currentInvested, True)
+    helper.printDict(yearlyProfits, "Gross profits:")
+    helper.printDict(yearlyTaxedProfits, "Net profits:")
+    helper.printDict(yearlyTransactions, "Transactions:")
+
 if __name__ == "__main__":
-    #TODO: move into test code
-    data = csvreader.read("./tests/sample.csv")
-    analyze(data)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csvDir', metavar='csvDir', type=str, help='CSV directory')
+    args = parser.parse_args()
+    doAnalyze(args)
--- analyzer/helper.py
+++ analyzer/helper.py
@@ -1,7 +1,17 @@
 from datetime import date
 from datetime import datetime
+import pprint
 
 DATE_FORMAT="%m/%d/%Y"
+
+def getYear(dateStr):
+    if dateStr:
+        return dateStr[-4:]
+    return _getCurrentYear()
+
+def _getCurrentYear():
+    #TODO: implement
+    return '2016'
 
 def getDays(startDate, endDate):
     start = _getDate(startDate)
@@ -13,3 +23,21 @@ def _getDate(dateVal):
     if not dateVal:
         return datetime.today()
     return datetime.strptime(dateVal, DATE_FORMAT)
+
+def printPercent(descr, num, denom, newLine=False):
+    print descr,
+    print "{percent:.2%}".format(percent=num/denom)
+    _printNewLine(newLine)
+
+def printFloat(descr, value, newLine=False):
+    print "%s: %.2f" % (descr, value)
+    _printNewLine(newLine)
+
+def printDict(dikt, descr, newLine=True):
+    print descr
+    pprint.pprint(dikt)
+    _printNewLine(newLine)
+
+def _printNewLine(newLine):
+    if newLine:
+        print ""
